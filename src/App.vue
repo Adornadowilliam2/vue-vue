1b0e1a1a77317cfc6dc4ffbee8ae182225bc6871
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-01-29 12:51:42 +0800

    monk content

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,9 @@
     <div class="card-container flex flex-wrap gap-4 justify-evenly p-4">
       <!-- Use gallery.image to display each image -->
       <div v-for="(gallery) in galleries" :key="gallery.id" class="card border border-black">
-        <img :src="gallery.image" class="w-full" alt="Gallery Image">
+        <img :src="gallery.image" class="w-full" alt="Gallery Image" >
         <h2>{{ gallery.name }}</h2>
-        <!-- Log each gallery object in the console for debugging -->
-        {{ console.log(gallery) }}
+   
       </div>
     </div>
   </main>
@@ -22,31 +21,31 @@ export default {
   data() {
     return {
       title: "Monk Gallery Photos",
-      galleries: [  // Changed from 'index' to 'galleries'
+      galleries: [  
         {
           id: 1,
           name: "Monk Shield the police",
-          image: "./assets/image1.png",
+          image: "https://github.com/Adornadowilliam2/vue-vue/blob/master/src/assets/image1.png?raw=true",
         },
         {
           id: 2,
-          name: "Monk firing smoke grenade",
-          image: "./assets/image2.png",
+          name: "Monk wearing a gas mask",
+          image: "https://github.com/Adornadowilliam2/vue-vue/blob/master/src/assets/image2.png?raw=true",
         },
         {
           id: 3,
-          name: "Monk attack the police",
-          image: "./assets/image3.png",
+          name: "Monk vs policies",
+          image: "https://github.com/Adornadowilliam2/vue-vue/blob/master/src/assets/image3.png?raw=true",
         },
         {
           id: 4,
-          name: "Image of monk and police",
-          image: "./assets/image4.png",
+          name: "Image of monk and police rally",
+          image: "https://github.com/Adornadowilliam2/vue-vue/blob/master/src/assets/image4.png?raw=true",
         },
         {
           id: 5,
           name: "The holy kick of monk",
-          image: "./assets/image5.png",
+          image: "https://github.com/Adornadowilliam2/vue-vue/blob/master/src/assets/image5.png?raw=true",
         }
       ]
     }
